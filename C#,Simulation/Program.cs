a7f28413e82b841f67c7f4c6b1821b6a6a04679a
AdrianQS2004 <adrian.quiros76@gmail.com>
2024-11-28 19:20:17 -0600

    Continued to try and do the out of order
    The main idea is there, but for some reason, it simply can't issue an instructions thats in the dictionary, maybe use the indexing?

--- C#,Simulation/Program.cs
+++ C#,Simulation/Program.cs
@@ -231,6 +231,7 @@ public class Processor
     public void Single_Instruction_run()
     {
         int instructionIndex = 0;
+        int _currentCycle = 0;
         int totalInstructions = _instructions.Count;
         Instruction? currentInstruction = null;
         int retireCycle = 0;
@@ -281,13 +282,11 @@ public class Processor
     private void Superscalar_in_order_run()
     {
         int instructionIndex = 0;
+        int _currentCycle = 0;
         int totalInstructions = _instructions.Count;
-        var currentInstructions = new Instruction[_issue_slots];
         int multipliedValue = _issue_slots;
         Dictionary<int, int> retireCycles = new Dictionary<int, int>();
         int IssuedColumn = 3 * multipliedValue;
-
-        bool[] stalled = new bool[_issue_slots];
 
         Console.WriteLine($"{"Cycle",-10}{"Issued Instruction",-10 * 3}{"Retired Instruction",-10 * 2}");
         Console.WriteLine(new string('-', 10 * (6)));
@@ -361,14 +360,13 @@ public class Processor
     private void Superscalar_out_of_order_run()
     {
         int instructionIndex = 0;
+        int _currentCycle = 0;
         int totalInstructions = _instructions.Count;
-        var currentInstructions = new Instruction[_issue_slots];
         int multipliedValue = _issue_slots;
         Dictionary<int, int> retireCycles = new Dictionary<int, int>();
-        int IssuedColumn = 3 * multipliedValue;
-        Dictionary<Instruction, int> InCycleinstructions = new Dictionary<Instruction, int>();
+        //int IssuedColumn = 3 * multipliedValue;
 
-        bool[] stalled = new bool[_issue_slots];
+        Dictionary<int, Instruction> instructionsWithDependency = new Dictionary<int, Instruction>();
 
         Console.WriteLine($"{"Cycle",-10}{"Issued Instruction",-10 * 3}{"Retired Instruction",-10 * 2}");
         Console.WriteLine(new string('-', 10 * (6)));
@@ -376,6 +374,13 @@ public class Processor
         // Run until all instructions are retired
         while (instructionIndex < totalInstructions || retireCycles.Count > 0)
         {
+
+            if (_currentCycle > 20)
+            {
+                Console.WriteLine("\nSurpassed 20 cycles, Loop is ending\n");
+                break;
+            }
+
             _currentCycle++;
             string issuedInstruction = "";
             string retiredInstruction = "";
@@ -383,6 +388,75 @@ public class Processor
             // Issue instructions if slots are free and not stalled
             for (int i = 0; i < _issue_slots; i++)
             {
+                //This condition says that if no instructions are being issued, including those in the instructionwithDependency dictionary, then the loop is broken
+                if (instructionIndex >= totalInstructions && instructionsWithDependency.Count() == 0)
+                    break;
+
+                //It should now check if an instruction with dependency is ready
+                //If any of these instructions is not ready, we continue with everything, it doesnt leave an empty issue slot behing
+
+                //This list allows us to remove instructions from the dependency instruction dictionary that have already been issued
+                List<int> InstructionsToRemove = new List<int>();
+
+                //This code only leaves an empty issue slot when the original run of the instructions has a dependency
+                //Console.WriteLine("\nTrying to go through the Instructions with dependency list");
+                //Console.WriteLine("Value of the amount of issue slots used: " + i + "\n");
+
+                //Will go eat
+                //It seems it is entering this even though the first it goes through this, it should be empty
+
+                foreach (KeyValuePair<int, Instruction> DependencyInstruction in instructionsWithDependency)
+                {
+
+                    Console.WriteLine("\n----------------\n");
+                    Console.WriteLine(" A dependency instruction: " + DependencyInstruction + " ");
+                    Console.WriteLine("\n----------------\n");
+
+                    //Console.WriteLine(" Value of i: " + i + " ----- Value of Issue Slots being used: " + _issue_slots + "\n");
+
+                    if (i < _issue_slots)
+                    {
+                        Console.WriteLine("\nAn instruction with dependencies is trying to be issued\n");
+
+                        _scheduler.PrintDictionaries();
+
+                        if (_scheduler.IsReady(DependencyInstruction.Value))
+                        {
+                            //If there are no dependencies, This means the instruction can be issued, which is then put in into the retire cyles
+                            //The retire Cycles dictionary tells us when an instruction is ready to be retired
+                            Console.WriteLine("\nAn instruction with dependencies is being issued\n");
+                            issuedInstruction += DependencyInstruction.Key + 1 + "." + DependencyInstruction.Value.ToString() + " ";
+                            retireCycles.Add(DependencyInstruction.Key, _currentCycle + DependencyInstruction.Value.CycleCost);
+                            i++;
+                        }
+                        else
+                        {
+                            // Dependency detected, continue progressing cycles
+                            //Console.WriteLine($"Cycle {_currentCycle}: Dependency detected for {instruction}");
+                            //break;
+                            Console.WriteLine("The instruction with dependencies still has dependencies\n");
+                        }
+
+                    }
+
+                }
+
+                foreach (var IssuedInstruction in InstructionsToRemove)
+                {
+                    instructionsWithDependency.Remove(IssuedInstruction); // Safely remove instructions from the dictionary after it was issued
+                }
+
+                // Console.WriteLine("\nSucessfully went through the Instructions with dependency list");
+
+                //Console.WriteLine("Value of the amount of issue slots used: " + i + "\n");
+
+                //Makes sure that if two or more issue slots have been used then we can't go in the normal instruction
+                if (i > _issue_slots)
+                {
+                    break;
+                }
+
+                //Console.WriteLine("Sucessfully went through the Instructions with dependency list\n");
 
                 if (instructionIndex >= totalInstructions)
                     break;
@@ -395,17 +469,23 @@ public class Processor
                     //If there are no dependencies, This means the instruction can be issued, which is then put in into the retire cyles
                     //The retire Cycles dictionary tells us when an instruction is ready to be retired
                     issuedInstruction += instructionIndex + 1 + "." + instruction.ToString() + " ";
-                    retireCycles.Add(instructionIndex + 1, _currentCycle + instruction.CycleCost);
-                    instructionIndex++;
+                    retireCycles.Add(instructionIndex, _currentCycle + instruction.CycleCost);
+
                 }
                 else
                 {
                     // Dependency detected, continue progressing cycles
                     //Console.WriteLine($"Cycle {_currentCycle}: Dependency detected for {instruction}");
-                    break;
+                    instructionsWithDependency.Add(instructionIndex, instruction);
+                    //break;
                 }
 
+                //Instruction index is summed up no matter what, specially because it is out of order
+                instructionIndex++;
+
             }
+
+            //Instructions are retired no matter how many are done or if they are in a different order
 
             List<int> cyclesToRemove = new List<int>();
 
@@ -416,17 +496,17 @@ public class Processor
                 if (_currentCycle == cycle.Value)
                 {
                     //Removes the numbers from the tables in the Scheduler, meaning the registers have been freed up
-                    _scheduler.RetireInstruction(_instructions[cycle.Key - 1]);
+                    _scheduler.RetireInstruction(_instructions[cycle.Key]);
 
                     //Sums the retired instructions into the variable that will be printed
-                    retiredInstruction += $"Instruction {cycle.Key} ";
+                    retiredInstruction += $"Instruction {cycle.Key + 1} ";
 
                     //Removes the retired instruction from retire cycles
                     cyclesToRemove.Add(cycle.Key);
                 }
             }
 
-            foreach (var index in cyclesToRemove.OrderByDescending(x => x))
+            foreach (var index in cyclesToRemove)
             {
                 retireCycles.Remove(index); // Safely remove cycles after enumeration
             }
@@ -459,8 +539,8 @@ public static class Program
 
         //Runs the processor with single instruction, in order, with one issue slot
         var processor = new Processor(instructions, 1, 1);
-        var processorInOrder = new Processor(instructions, 2, 2);
-        //var processorOutofOrder = new Processor(instructions, 3, 1);
+        var processorInOrder = new Processor(instructions, 2, 1);
+        var processorOutofOrder = new Processor(instructions, 3, 1);
 
     }
 
--- C#,Simulation/obj/Debug/net8.0/C#,Simulation.AssemblyInfo.cs
+++ C#,Simulation/obj/Debug/net8.0/C#,Simulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#,Simulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4649275e2e3b10e3f70408bdda9600c23e8420d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ebee7bd9e94bf138198d73d2e23dad5aa0058b")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#,Simulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#,Simulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
